49c5c57670c3ca730bf32c5c18d9ebd59a89a73c
omarzedan20 <omarzcb@gmail.com>
2024-09-13 20:11:17 +0300

    Add project files.

---
+++ MVC_Project.BLL/Repositores/DepartmentRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_Project.BLL.Interfacies;
+using MVC_Project.DAL.Data;
+using MVC_Project.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Project.BLL.Repositores
+{
+    public class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly AppDbContext _dbContext;
+        public DepartmentRepository(AppDbContext dbComtext)    // Ask CLR For Create Object from DbContext
+        {
+            // _dbContext = new AppDbContext();
+            // 
+            _dbContext = dbComtext;   
+        }
+        public int Add(Department department)
+        {
+            _dbContext.Departments.Add(department); 
+            return _dbContext.SaveChanges();
+        }
+
+        public int Delete(Department department)
+        {
+            _dbContext.Departments.Remove (department);
+            return _dbContext.SaveChanges();
+        }
+
+        public IEnumerable<Department> GetAll()
+        {
+            return _dbContext.Departments.AsNoTracking().ToList();
+        }
+
+        public Department GetById(int id)
+        {
+            return _dbContext.Departments.Find(id);
+        }
+
+        public int Update(Department department)
+        {
+            _dbContext.Departments.Update(department);
+            return _dbContext.SaveChanges();
+        }
+    }
+}
